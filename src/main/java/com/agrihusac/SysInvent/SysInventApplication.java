a44e0fe425ab1d73873c779985b958ca1f45af88
ANDRES RICARDO YBAÑEZ QUISPE <andresricardoybanezquispe@gmail.com>
2026-09-23 01:50:07 +0000

    "Start : Structura Spring"

---
+++ src/main/java/com/agrihusac/SysInvent/SysInventApplication.java
@@ -0,0 +1,13 @@
+package com.agrihusac.SysInvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysInventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysInventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrihusac/SysInvent/SysInventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrihusac.SysInvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysInventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
